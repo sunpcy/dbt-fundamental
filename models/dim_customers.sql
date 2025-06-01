20093fe442a23798f64fa4dbd79ff98e4bfbbdf1
Prachya Kusumamarn <prachya.kusumamarn@gmail.com>
2025-06-01 21:02:27 +0000

    Learning about Modularity and the Ref Function

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -6,24 +6,15 @@
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from sun-dbt-learning.jaffle_shop.customers
+select *
+from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    
-    from sun-dbt-learning.jaffle_shop.orders
+select *
+from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
---
+++ models/stg_jaffle_shop__customers.sql
@@ -0,0 +1,6 @@
+select
+    id as customer_id,
+    first_name,
+    last_name
+
+from sun-dbt-learning.jaffle_shop.customers
